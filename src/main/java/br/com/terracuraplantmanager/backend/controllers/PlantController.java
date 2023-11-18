0d2114977343819e6e316a711524bd6c29f6264f
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-11-18 10:01:04 -0300

    feat: endpoint of plants  receiving json

--- src/main/java/br/com/terracuraplantmanager/backend/controllers/PlantController.java
+++ src/main/java/br/com/terracuraplantmanager/backend/controllers/PlantController.java
@@ -1,7 +1,12 @@
 package br.com.terracuraplantmanager.backend.controllers;
 
+import br.com.terracuraplantmanager.backend.model.PlaceModel;
 import br.com.terracuraplantmanager.backend.model.PlantModel;
+import br.com.terracuraplantmanager.backend.model.TaxonomyModel;
+import br.com.terracuraplantmanager.backend.model.UserModel;
+import br.com.terracuraplantmanager.backend.service.PlaceService;
 import br.com.terracuraplantmanager.backend.service.PlantService;
+import br.com.terracuraplantmanager.backend.service.TaxonomyService;
 import br.com.terracuraplantmanager.backend.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -19,7 +25,12 @@ import java.util.stream.Collectors;
 public class PlantController {
     @Autowired
     private PlantService plantService;
-
+    @Autowired
+    private PlaceService placeService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TaxonomyService taxonomyService;
     @GetMapping
     public ResponseEntity<List<PlantModel>> obterTodos() {
         List<PlantModel> plants = plantService.findAll();
@@ -31,15 +42,45 @@ public class PlantController {
     }
 
     @PostMapping
+
     public ResponseEntity<PlantModel> adicionar(@RequestBody PlantModel plantReq) {
         ModelMapper mapper = new ModelMapper();
 
-        PlantModel plant = mapper.map(plantReq, PlantModel.class);
+        // Create a new PlantModel instance
+        PlantModel plant = new PlantModel();
 
+        // Set simple properties directly
+        plant.setPlantName(plantReq.getPlantName());
+        plant.setPlantDescription(plantReq.getPlantDescription());
+        plant.setPlant_access_token(plantReq.getPlant_access_token());
+
+        // Fetch and set UserModels
+        List<UserModel> users = new ArrayList<>();
+        for (UserModel userDto : plantReq.getUserModel()) {
+            UserModel user = userService.findById(userDto.getId()).orElse(null); // Replace with your service method
+            users.add(user);
+        }
+        plant.setUserModel(users);
+
+        // Fetch and set PlaceModels
+        List<PlaceModel> places = new ArrayList<>();
+        for (PlaceModel placeDto : plantReq.getPlaceModel()) {
+            PlaceModel place = placeService.findById(placeDto.getId()).orElse(null); // Replace with your service method
+            places.add(place);
+        }
+        plant.setPlaceModel(places);
+
+        // Fetch and set TaxonomyModel
+        TaxonomyModel taxonomy = taxonomyService.findById(plantReq.getTaxonomyModel().getId()).orElse(null); // Replace with your service method
+        plant.setTaxonomyModel(taxonomy);
+
+        // Save the plant
         plant = plantService.create(plant);
 
+        // Return the response
         return new ResponseEntity<>(mapper.map(plant, PlantModel.class), HttpStatus.CREATED);
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Optional<PlantModel>> obterPorId(@PathVariable Long id) {
